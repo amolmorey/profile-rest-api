5213beeef339c822577d78ccc2e5c9deb87be7b0
aditya0210 <kasyapaditya777@gmail.com>
2021-07-16 09:56:05 +0530

    models.py last_name added 5

--- profile_api/models.py
+++ profile_api/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-	user1 = models.CharField(max_length=100)
+	user = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
 	name = models.CharField(max_length=200)
 	last_name = models.CharField(max_length=200)
 	fiest = models.CharField(max_length=200)
